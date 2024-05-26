bfabb7e42a00d118b8f50839b3446b23ebb7ffdb
AyushyaChongder <ayushyachongder@gmail.com>
2024-05-26 14:28:10 +0530

    Register form completed

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,19 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Register from "./Register";
+import { useForm } from "react-hook-form";
 
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
+
   return (
     <>
       <div>
-        <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+        <dialog id="my_modal_5" className="modal sm:modal-middle">
           <div className="modal-box dark:bg-slate-100 light:bg-white ">
+          <form method="dialog" className="modal-backdrop">
+          <Link to="/" className="text-pink-500">
+                    X
+                  </Link>
+          </form>
             <h3 className="font-bold text-2xl">
               Welcome <span className="text-pink-500">User!!</span>
             </h3>
             <h5 className="py-4">Please enter your login credentials</h5>
 
-            <form method="dialog">
+            <form onSubmit={handleSubmit(onSubmit)} method="dialog" >
               <div>
                 <span>Email</span>
                 <br />
@@ -21,26 +34,32 @@ function Login() {
                   type="email"
                   placeholder="Enter your email"
                   className="w-full px-3 py-3 my-3 border-slate-300 rounded-md dark:bg-slate-200 light:bg-white"
+                  {...register("email", { required: true })}
                 />
-                <br />
+                {errors.email && <span className="text-sm text-pink-500">This field is required</span>}
+                <br /><br />
                 <span>Password</span>
                 <br />
                 <input
                   type="password"
                   placeholder="Enter your password"
                   className="w-full px-3 py-3 my-3 border-slate-300 rounded-md dark:bg-slate-200 light:bg-white"
+                  {...register("password", { required: true })}
                 />
+                {errors.password && <span className="text-sm text-pink-500">This field is required</span>}
               </div>
               <div className="flex">
                 <button className="btn btn-md btn-secondary my-4 px-7 dark:text-white">
                   Log in
                 </button>
-                <p className="ml-5 mt-8">Not Registered?<Link to="/register" className="text-pink-500 underline"> Register</Link></p>
+                <p className="ml-5 mt-8">Not Registered? <Link to="/register" className="text-pink-500 underline"> Register</Link></p>
               </div>
             </form>
           </div>
           <form method="dialog" className="modal-backdrop">
-            <button>close</button>
+          <button>
+                    close
+                  </button>
           </form>
         </dialog>
       </div>
--- Frontend/src/components/Register.jsx
+++ Frontend/src/components/Register.jsx
@@ -1,17 +1,34 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import Login from "./Login";
+import { useForm } from "react-hook-form";
+
 
 function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
+
   return (
     <>
-      <div className="flex items-center justify-center mt-20">
-        <div id="my_modal_6" >
-          <div className="modal-box dark:bg-slate-100 light:bg-white w-full ">
+      <div className="flex items-center justify-center mt-10">
+        <div id="my_modal_6" className="w-[600px] modal-middle" >
+          <div className="modal-box  dark:bg-slate-100 light:bg-white ">
+            <form method="dialog" className="modal-backdrop">
+          <Link to="/" className="text-pink-500">
+                    X
+                  </Link>
+          </form>
             <h3 className="font-bold text-2xl">
               Welcome <span className="text-pink-500">User!!</span>
             </h3>
             <h5 className="py-4">Please enter your details</h5>
 
-            <form method="dialog">
+            <form onSubmit={handleSubmit(onSubmit)} method="dialog">
               <div>
               <span>Name</span>
                 <br />
@@ -19,24 +36,32 @@ function Register() {
                   type="text"
                   placeholder="Enter your name"
                   className="w-full px-3 py-3 my-3 border-slate-300 rounded-md dark:bg-slate-200 light:bg-white"
+                  {...register("name", { required: true })}
                 />
                 <br />
+                {errors.name && <span className="text-sm text-pink-500">This field is required</span>}
+                <br /><br />
                 <span>Email</span>
                 <br />
                 <input
                   type="email"
                   placeholder="Enter your email"
                   className="w-full px-3 py-3 my-3 border-slate-300 rounded-md dark:bg-slate-200 light:bg-white"
+                  {...register("email", { required: true })}
                 />
                 <br />
+                {errors.email && <span className="text-sm text-pink-500">This field is required</span>}
+                <br /><br />
                 <span>Password</span>
                 <br />
                 <input
                   type="password"
                   placeholder="Enter your password"
                   className="w-full px-3 py-3 my-3 border-slate-300 rounded-md dark:bg-slate-200 light:bg-white"
+                  {...register("password", { required: true })}
                 />
                 <br />
+                {errors.password && <span className="text-sm text-pink-500">This field is required</span>}
                 
               </div>
               <div className="flex">
@@ -45,16 +70,16 @@ function Register() {
                 </button>
                 <p className="ml-5 mt-8">
                   Aready Registered?
-                  <span className="text-pink-500 underline">
-                    <a href=""> Login</a>
-                  </span>
+                  <button className="text-pink-500 underline"
+                  onClick={()=>document.getElementById('my_modal_5').showModal()}>
+                     Login
+                  </button>
+                  <Login/>
                 </p>
               </div>
             </form>
           </div>
-          <form method="dialog" className="modal-backdrop">
-            <button>close</button>
-          </form>
+          
         </div>
       </div>
     </>
